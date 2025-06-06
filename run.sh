fdf932e0c2c018beca06ad053d4236c8c4304493
dongguangping <1069851669@qq.com>
2025-06-06 13:09:15 +0800

    修改docker启动命令和初始化

--- run.sh
+++ run.sh
@@ -6,6 +6,8 @@ docker run -itd --name=baidu-pcs \
 	-p 8081:8080 \
 	-e BDUSS=xxx \
 	-e STOKEN=xxx \
+	-e AUTH=true \
+	-e TOKEN=1q2w3e4r5t \
 	-v ./data/Downloads:/root/Downloads \
 	-v ./data/config:/root/.config/BaiduPCS-Go \
  	baidupcs:latest
--- startup.sh
+++ startup.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
 # 登录环境变量传入的账号
-/app/BaiduPCS-Go login --bduss=${BDUSS} --stoken=${STOKEN}
+#如果文件夹不存在，创建文件夹
+if [ ! -d "/root/.config/BaiduPCS-Go" ]; then
+  echo "开始初始化!"
+  /app/BaiduPCS-Go login --bduss=${BDUSS} --stoken=${STOKEN}
+else
+  echo "查询到配置文件!"
+  echo "跳过初始化!"
+fi
 
 # 启动服务
-/app/BaiduPCS-Go srv
+if [ ! $AUTH ]; then
+  echo "未开启 secret 认证!"
+  /app/BaiduPCS-Go srv
+else
+  echo "已开启 secret 认证!"
+  echo "secret: $TOKEN"
+  /app/BaiduPCS-Go srv --auth --secret=$TOKEN
+fi
